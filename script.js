41731d6c43011abdd7b79a3a0e62904c677f1a44
Aanchal <159253330+AchlMaurya@users.noreply.github.com>
2025-02-22 19:27:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const audio = document.getElementById('audio');
+        
+        function playAudio() {
+            audio.play();
+        }
+        
+        function pauseAudio() {
+            audio.pause();
+        }
+        
+        function stopAudio() {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+        
+        function setVolume() {
+            audio.volume = document.getElementById('volume').value;
+        }
